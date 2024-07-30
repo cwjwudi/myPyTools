983d0333882b974c12040db460f96e9d32333636
Wenjie Cui <wenjie.cui@br-automation.com>
2024-07-30 14:46:06 +0800

    重组文件

--- myMPC/pid_gym.py
+++ myMPC/pid_gym.py
@@ -1,53 +1,58 @@
 import gym
-
-from gym.envs.mujoco.inverted_pendulum_v4 import InvertedPendulumEnv
-import time
 import numpy as np
+import keyboard
+
+from env.InvertedPendulumEnv import InvertedPendulumEnv_Ctrl, InvertedPendulumEnv_Ctrl2
 
 
-class InvertedPendulumEnv_Ctrl(InvertedPendulumEnv):
-    def step(self, a):
-        reward = 1.0
-        self.do_simulation(a, self.frame_skip)
-        ob = self._get_obs()
-        terminated = bool(not np.isfinite(ob).all() or (np.abs(ob[1]) > 0.5))
-        if self.render_mode == "human":
-            self.render()
-        return ob, reward, terminated, False, {}
 
+if __name__ == "__main__":
+    # 生成环境
+    # env = InvertedPendulumEnv_Ctrl2(cfg="./env/config.yaml")
+    env = InvertedPendulumEnv_Ctrl(render_mode="human")
 
-# 生成环境
-env = InvertedPendulumEnv_Ctrl(render_mode="human")
-env.init_qpos = 0.2
-# env = gym.make('InvertedPendulum-v4',  render_mode="human")
-# 环境初始化
-state = env.reset()
-state = state[0]
-# 循环交互
-while True:
-    # 渲染画面
-    env.render()
-    # 从动作空间随机获取一个动作
-    pos_pole = state[1]
-    vel_pole = state[3]
+    env.init_qpos = 0.2
+    # 环境初始化
+    state = env.reset()
+    state = state[0]
+    # 设置完成时间，单位s
+    done_time = 10.0
+    total_time = 0.0
+    # 循环交互
+    while True:
+        # render
+        env.render()
 
-    pos_cart = state[0]
-    vel_cart = state[2]
+        # Controller
+        pos_pole = state[1]
+        vel_pole = state[3]
+        pos_cart = state[0]
+        vel_cart = state[2]
 
-    u_pole = 5*(pos_pole - 0) + 0.11*vel_pole
-    u_cart = 1*(pos_cart - 0) + 0.2*vel_cart
-    action = u_pole + u_cart
-    action = np.array([action])
+        u_pole = 5 * (pos_pole - 0) + 0.11 * vel_pole
+        u_cart = 1 * (pos_cart - 0) + 0.2 * vel_cart
+        action = u_pole + u_cart
+        action = np.clip(action, -3, 3)
+        action = np.array([action])
 
+        # contact with the env
+        state, reward, done, _, info = env.step(action)
+        print('state = {0}; reward = {1}'.format(state, reward))
 
-    # action = env.action_space.sample()
-    # agent与环境进行一步交互
-    state, reward, done, _, info = env.step(action)
-    print('state = {0}; reward = {1}'.format(state, reward))
-    # 判断当前episode 是否完成
-    if done:
-        print('done')
-        break
-    # time.sleep(1)
-# 环境结束
-env.close()
+        # 计算是否达到done时间
+        total_time = total_time + env.dt
+        if total_time > done_time:
+            done = True
+
+        # 判断当前episode 是否完成
+        if done:
+            print('done')
+            break
+
+        # 检查是否按下 'q' 键
+        if keyboard.is_pressed('q'):
+            print('Exiting...')
+            break
+
+    # 环境结束
+    env.close()
